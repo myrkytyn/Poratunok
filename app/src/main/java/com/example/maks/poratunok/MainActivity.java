006c8f3beced773173c25699f835c19ba72dd782
Maks <makcm007@gmail.com>
2019-03-03 17:44:48 +0200

    3 taps and phone

--- app/src/main/java/com/example/maks/poratunok/MainActivity.java
+++ app/src/main/java/com/example/maks/poratunok/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.maks.poratunok;
 
+import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,10 +17,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //дозвіл на дзвінок
+    private static final int REQUEST_CALL=1;
     //оголошую кнопку і текст
     TextView tvText;
     ImageButton btnHelp;
     TextView tvText2;
+    TextView tvText3;
+    int i = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,18 +34,49 @@ public class MainActivity extends AppCompatActivity {
         //Пошук елеменів
         tvText = (TextView) findViewById(R.id.tvText);
         tvText2 = (TextView) findViewById(R.id.tvText2);
+        tvText3 = (TextView) findViewById(R.id.tvText3);
         btnHelp = (ImageButton) findViewById(R.id.btnHelp);
 
-        //опрацьовувач натискання основної кнопки
-        View.OnClickListener oclBtnHelp = new View.OnClickListener() {
+        //кнопка
+        btnHelp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Змінюю текст у tvText
-                tvText.setVisibility(View.INVISIBLE);
-                tvText2.setVisibility(View.VISIBLE);
+                //перше натискання
+                if(i==0){
+                    tvText.setVisibility(View.INVISIBLE );
+                    tvText2.setVisibility(View.VISIBLE);
+                    i=1;
+                //друге натискання
+                }else if(i==1){
+                    tvText.setVisibility(View.INVISIBLE );
+                    tvText2.setVisibility(View.INVISIBLE);
+                    tvText3.setVisibility(View.VISIBLE);
+                    i=2;
+                //третє натискання
+                }else if(i==2){
+                    makeCall();
+                }
             }
-        };
-        // присвоюю опрацьовувач кнопці
-        btnHelp.setOnClickListener(oclBtnHelp);
+        });
+    }
+
+    private void makeCall() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[] {Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+        }else {
+            String dial = "tel:"+"0986455492";
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == REQUEST_CALL){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                makeCall();
+            }
+        }
     }
 }
